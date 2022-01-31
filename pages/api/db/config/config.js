ea38664bb87d35647561a7b0bd1bbfede0efd7bc
septe01 <septe.vhirgo@gmail.com>
2022-01-31 07:31:10 +0700

    setup config add procfile

--- pages/api/db/config/config.js
+++ pages/api/db/config/config.js
@@ -19,14 +19,15 @@ module.exports = {
     logging: false,
   },
   production: {
-    use_env_variable: "DATABASE_URL",
+    username: "wnwsgyylcrqxpp",
+    password:
+      "47cf1491c8a32ed54c0b8a46403c167d166018a015c82c393faf490139d224c3",
+    database: "daio0l02l69oh5",
+    host: "ec2-52-208-221-89.eu-west-1.compute.amazonaws.com",
+    port: "5432",
     dialect: "postgres",
-    protocol: "postgres",
     dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // <<<<<<< YOU NEED THIS
-      },
+      ssl: true,
     },
   },
 };
